53cd234f98aa24535096874b9e34ce7896c43f86
Seilkhan Rakhatali <35730436+RSeilkhan@users.noreply.github.com>
2020-04-04 11:48:46 +0600

    Add files via upload

---
+++ LastTry/LastTry/Controllers/MessagesController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LastTry.Controllers
+{
+    /*    [Route("Say")]
+        public class MessagesController : Controller
+        {
+            [Route("{**message}/{}")] //выдает ошибку что нельзя использовать {} в route, если их убрать то не работает
+            public IActionResult ShowMessage(string message)
+            {
+                if (string.IsNullOrEmpty(message))
+                {
+                    ViewData["Message"] = "Message is empty";
+                }
+                else
+                {
+                    ViewData["Message"] = message;
+                }
+
+                return View();
+            }
+        }
+    */
+    public class MessagesController : Controller
+    {
+        public IActionResult ShowMessage(string message)
+        {
+            if (string.IsNullOrEmpty(message))
+            {
+                ViewData["Message"] = "Message is empty";
+            }
+            else
+            {
+                ViewData["Message"] = message;
+            }
+
+            return View();
+        }
+    }
+}
